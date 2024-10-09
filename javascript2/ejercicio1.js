fed2065b2229afae1febca653d8fe10e8576690d
Renato2 <blurry.erik@alumnos.alborfp.com>
2024-10-09 22:26:05 +0200

    Terminar ejercicio1.js. Añadir ejercicio2.js.

---
+++ javascript2/ejercicio1.js
@@ -0,0 +1,17 @@
+//Ejercicio 1
+
+alert("Vamos a calcular los gastos totales del viaje que vas a realizar.");
+
+let alojamiento = Number(prompt("Por favor, ingresa el coste del alojamiento: "));
+let alimentacion = Number(prompt("Por favor, ingresa el coste de la alimentación: "));
+let entretenimiento = Number(prompt("Por favor, ingresa el coste del entretenimiento: "));
+
+costeTotal = alojamiento + alimentacion + entretenimiento;
+
+if (costeTotal % 1 == 0) { 
+  alert("El coste total del viaje es: " + costeTotal + "€.");
+  console.log("El coste total del viaje es: " + costeTotal + "€.")
+} else {
+  alert("El coste total del viaje es: " + costeTotal.toFixed(2) + "€.");
+  console.log("El coste total del viaje es: " + costeTotal.toFixed(2) + "€.")
+}
