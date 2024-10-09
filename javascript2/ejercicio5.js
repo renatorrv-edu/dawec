f498248e003d8846f475850f9d27130c8f06a2e4
renatorrv-edu <rromval599@fp.iesromerovargas.com>
2024-10-09 12:33:56 +0200

    Añadir ejercicio5.

---
+++ javascript2/ejercicio5.js
@@ -0,0 +1,25 @@
+function esPrimo(x) {
+    if (x < 2) {
+        return false;
+    }
+    
+    for (let i = 2; i < x; i++) {
+        if (x % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+let comienzo = 5, final = 47;
+
+let numerosPrimos = [];
+
+for (let i = comienzo; i <= final; i++) {
+    if(esPrimo(i)) {
+        numerosPrimos.push(i);
+    }
+}
+
+console.log(numerosPrimos);
