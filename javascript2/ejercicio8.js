a2e4737c7c4f11ee9f3cfd1490bcdc626428a2c4
renatorrv-edu <rromval599@fp.iesromerovargas.com>
2024-10-10 14:13:31 +0200

    Añadir ejercicio8 sin terminar.

--- javascript2/ejercicio8.js
+++ javascript2/ejercicio8.js
@@ -1,2 +1,16 @@
 //Ejercicio 8
 
+function calculateFactorial (n) {
+
+  let result = 1;
+
+  if (n == 1) {
+    return result;
+  }
+
+  for (let i = 1; i >= n; i++) {
+    result *= i;
+  }
+}
+
+alert("");
