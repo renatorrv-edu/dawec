cf66395efa56cd88514c79f7638a59b433ca81ca
Renato2 <blurry.erik@alumnos.alborfp.com>
2024-10-11 02:34:17 +0200

    Terminar ejercicio8 y editar ejercicio5.

--- javascript2/ejercicio5.js
+++ javascript2/ejercicio5.js
@@ -2,7 +2,7 @@ function esPrimo(x) {
     if (x < 2) {
         return false;
     }
-    
+
     for (let i = 2; i < x; i++) {
         if (x % i == 0) {
             return false;
@@ -12,7 +12,27 @@ function esPrimo(x) {
     return true;
 }
 
-let comienzo = 5, final = 47;
+alert("Vamos a calcular los números primos en un intervalo que tú vas a definir.");
+
+let comienzo = parseInt(prompt("Por favor, introduce un número entero desde la que comience la secuencia: "));
+
+while (Number.isNaN(comienzo)) {
+    alert("Entrada inválida.");
+    comienzo = parseInt(prompt("Por favor, introduce un número entero desde la que comience la secuencia: "));
+}
+
+let final = parseInt(prompt("Por favor, introduce un número entero que marque el final de la secuencia: "));
+
+while (Number.isNaN(final)) {
+    alert("Entrada inválida.");
+    final = parseInt(prompt("Por favor, introduce un número entero que marque el final de la secuencia: "));
+}
+
+if (final < comienzo) { //Dejo esta pequeña comprobación en caso de que el usuario haya puesto los números al revés.
+    let aux = final;
+    comienzo = final;
+    final = aux;
+}
 
 let numerosPrimos = [];
 
@@ -22,6 +42,10 @@ for (let i = comienzo; i <= final; i++) {
     }
 }
 
-console.log(numerosPrimos);
-
-//Revisar y rehacer.
+if (numerosPrimos.length === 0) { //Pongo esto en caso de que no se hayan encontrado números primos en la secuencia.
+    alert("No se encontraron números primos en la secuencia.");
+    console.log("No se encontraron números primos en la secuencia.");
+} else {
+    alert("Los números primos encontrados en la secuencia son: " + numerosPrimos + ".");
+    console.log("Los números primos encontrados en la secuencia son: " + numerosPrimos + ".");
+}
--- javascript2/ejercicio8.js
+++ javascript2/ejercicio8.js
@@ -4,13 +4,21 @@ function calculateFactorial (n) {
 
   let result = 1;
 
-  if (n == 1) {
-    return result;
-  }
-
-  for (let i = 1; i >= n; i++) {
+  for (let i = 1; i <= n; i++) {
     result *= i;
   }
+
+  return result;
 }
 
-alert("");
+alert("Vamos a calcular el factorial de un número.");
+
+let n = parseInt(prompt("Por favor, introduce un número entero positivo."));
+
+while (Number.isNaN(n) || n < 0) {
+  alert("Entrada inválida.");
+  n = parseInt(prompt("Por favor, introduce un número entero positivo."));
+}
+
+alert("El factorial de " + n + " equivale a: " + calculateFactorial(n) + ".");
+console.log("El factorial de " + n + " equivale a: " + calculateFactorial(n) + ".");
