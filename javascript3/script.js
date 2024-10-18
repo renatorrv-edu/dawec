9da40296d4f6b31c9864246d8e1ec1ac9bfbfd51
renatorrv-edu <rromval599@fp.iesromerovargas.com>
2024-10-18 12:00:52 +0200

    Añadir ejercicios 3.

---
+++ javascript3/script.js
@@ -0,0 +1,9 @@
+// Ejercicio 1
+
+// Ejercicio 2
+console.log("hola")
+let divEjercicio2 = document.getElementById('divHover');
+divEjercicio2.addEventListener('mouseover', function (){
+  let div2 = document.getElementById('efecto');
+  div2.style.background = 'green';
+});
