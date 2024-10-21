fb1c7aa60a7b3a2e0fdcde6d04f62ec6d4c9b4f2
Renato2 <blurry.erik@alumnos.alborfp.com>
2024-10-21 20:31:27 +0200

    Añadir ejercicio 3 y 4.

--- javascript3/script.js
+++ javascript3/script.js
@@ -1,6 +1,8 @@
 // Ejercicio 1
+// Incluído en el archivo HTML.
 
 // Ejercicio 2
+// Incluído en el archivo HTML.
 
 let div2_1 = document.getElementById('divHover');
 
@@ -17,4 +19,21 @@ function colorBlanco() {
   div2_2.style.background = '#FFFFFF';
 }
 
-//Prueba.
+// Ejercicio 3
+
+let div3_precio = document.getElementById('precio');
+let div3_cantidad = document.getElementById('cantidad');
+let div3_calcular = document.getElementById('enviar');
+let div3_total = document.getElementById('total');
+
+div3_calcular.addEventListener('click', () => calcular(div3_precio.value, div3_cantidad.value));
+
+function calcular(n, m) {
+  div3_total.value = n * m;
+}
+
+// Ejercicio 4
+// Incluído en el HTML.
+
+// Ejercicio 5
+
