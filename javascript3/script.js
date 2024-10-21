b3acb817c89001f8db3cdfb0344f598042b9d125
Renato2 <blurry.erik@alumnos.alborfp.com>
2024-10-21 19:30:56 +0200

    Modificar ejercicio 3.

--- javascript3/script.js
+++ javascript3/script.js
@@ -1,9 +1,18 @@
 // Ejercicio 1
 
 // Ejercicio 2
-console.log("hola")
-let divEjercicio2 = document.getElementById('divHover');
-divEjercicio2.addEventListener('mouseover', function (){
-  let div2 = document.getElementById('efecto');
-  div2.style.background = 'green';
-});
+
+let div2_1 = document.getElementById('divHover');
+
+div2_1.addEventListener('mouseover', color);
+div2_1.addEventListener('mouseleave', colorBlanco)
+
+function color() {
+  let div2_2 = document.getElementById('efecto');
+  div2_2.style.background = '#A6E999';
+}
+
+function colorBlanco() {
+  let div2_2 = document.getElementById('efecto');
+  div2_2.style.background = '#FFFFFF';
+}
