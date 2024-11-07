2ebf0264d258644a86e66ee260035a2ea1f8c2ef
renatorrv-edu <renatorrv-edu@github.com>
2024-11-07 13:42:31 +0100

    Añadir ejercicio1.

---
+++ javascript5/script.js
@@ -0,0 +1,9 @@
+// Ejercicio 1
+let boton1 = document.getElementById("boton1")
+
+boton1.addEventListener(click, function(){
+  let diasSemana  = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
+  alert("Por favor, introduce un número:")
+  let dia = Number(prompt("Mete un número"))
+  alert("asd")
+})
