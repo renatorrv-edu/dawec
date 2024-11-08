50da917256f70e7fa01cd7c42edd3841dfa10495
Renato2 <blurry.erik@alumnos.alborfp.com>
2024-11-08 01:09:35 +0100

    Fix script.js

--- javascript5/script.js
+++ javascript5/script.js
@@ -1,7 +1,7 @@
 // Ejercicio 1
-let boton1 = document.getElementById("boton1")
+let boton1 = document.getElementById("ejercicio1")
 
-boton1.addEventListener(click, function(){
+boton1.addEventListener('click', function(){
   let diasSemana  = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
   alert("Por favor, introduce un número:")
   let dia = Number(prompt("Mete un número"))
