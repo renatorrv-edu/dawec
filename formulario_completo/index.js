258c4d12f5ef0d90652c87e59d83e56b78ff25c6
Renato <rromval599@g.educaand.es>
2024-12-11 21:57:10 +0100

    Terminar ejercicio.

--- formulario_completo/index.js
+++ formulario_completo/index.js
@@ -1,33 +1,21 @@
-// Recopilamos las áreas de input del formulario.
+// Recopilamos las áreas de input del formulario que vamos a utilizar.
 
 const form = document.getElementById('form');
 const nameInput = document.getElementById('name');
 const firstLastNameInput = document.getElementById('first_lastname');
 const secondLastNameInput = document.getElementById('second_lastname');
-const addressInput = document.getElementById('address');
 const phoneInput = document.getElementById('phone');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
-const sexV = document.getElementById('sex_v');
-const sexM = document.getElementById('sex_m')
-const nivelEstudios1 = document.getElementById('nivel_estudios_1');
-const nivelEstudios2 = document.getElementById('nivel_estudios_2');
-const nivelEstudios3 = document.getElementById('nivel_estudios_3');
-const nivelEstudios4 = document.getElementById('nivel_estudios_4');
-const nivelEstudios5 = document.getElementById('nivel_estudios_5');
-const articulos1 = document.getElementById('articulos_1');
-const articulos2 = document.getElementById('articulos_2');
-const articulos3 = document.getElementById('articulos_3');
-const articulos4 = document.getElementById('articulos_4');
-const articulos5 = document.getElementById('articulos_5');
-const frecuencia = document.getElementById('frecuencia');
-const articuloArchivo = document.getElementById('articulo_archivo');
 const comentario = document.getElementById('comentario');
-const btnBorrar = document.getElementById('borrar_formulario');
+const borrarBtn = document.getElementById('borrar_formulario');
 
- 
-let regExName = (a);
-let 
+// Expresiones regulares que usaremos para las comprobaciones.
+let regExNames = new RegExp(/^[A-Z][a-z]+(?:\s[A-Z][a-z]+)*$/);
+let regExPasswords = new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)[A-Za-z\d]{6,10}$/);
+let regExEmails = new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/);
+let regExPhones = new RegExp(/^\d{3}\s\d{2}\s\d{2}\s\d{2}$/);
+let regExComments = new RegExp(/^.{0,200}$/);
 
 // Separamos la función de marcar el PrimerError para no repetir tanto código.
 function marcarPrimerError (input) {
@@ -38,7 +26,7 @@ function marcarPrimerError (input) {
 // Función de validación del formulario. Enviará true o false dependiendo del resultado de las validaciones.
 function validacion() {
   // Con esta variable almacenaremos los mensajes de error.
-  let mensajeError = 'Se han encontrado errores: \n';
+  let mensajeError = 'Se han encontrado los siguientes errores: \n\n';
 
   // Bandera con la que marcaremos qué campo es el primero al que volver si hay errores.
   let primerError = false;
@@ -49,13 +37,171 @@ function validacion() {
   // Información personal
 
   if (!nameInput.value) {
-    mensajeError += 'El campo "Nombre" no puede estar vacío.';
+    mensajeError += '- El campo "Nombre" no puede estar vacío.\n';
 
     if (!primerError) {
       marcarPrimerError(nameInput);
       primerError = true;
     }
+
+    esValido = false;
+  } else if (!regExNames.test(nameInput.value)) {
+    mensajeError += '- El nombre debe componerse por letras y comenzar con una mayúscula.\n';
+    
+    if (!primerError) {
+      marcarPrimerError(nameInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  }
+
+  if (!firstLastNameInput.value) {
+    mensajeError += '- El campo "Primer apellido" no puede estar vacío.\n';
+
+    if (!primerError) {
+      marcarPrimerError(firstLastNameInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  } else if (!regExNames.test(firstLastNameInput.value)) {
+    mensajeError += '- El primer apellido debe componerse por letras y comenzar con una mayúscula.\n';
+    
+    if (!primerError) {
+      marcarPrimerError(firstLastNameInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  }
+
+  if (!secondLastNameInput.value) {
+    mensajeError += '- El campo "Segundo apellido" no puede estar vacío.\n';
+
+    if (!primerError) {
+      marcarPrimerError(secondLastNameInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  } else if (!regExNames.test(secondLastNameInput.value)) {
+    mensajeError += '- El segundo apellido debe componerse por letras y comenzar con una mayúscula.\n';
+    
+    if (!primerError) {
+      marcarPrimerError(secondLastNameInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  }
+  
+  if (!phoneInput.value) {
+    mensajeError += '- El campo "Teléfono" no puede estar vacío.\n';
+
+    if (!primerError) {
+      marcarPrimerError(phoneInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  } else if (!regExPhones.test(phoneInput.value)) {
+    mensajeError += '- El teléfono debe estar compuesto solo por números en el siguiente formato: 123 33 33 33.\n';
+    
+    if (!primerError) {
+      marcarPrimerError(phoneInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  }
+
+  if (!emailInput.value) {
+    mensajeError += '- El campo "Email" no puede estar vacío.\n';
+
+    if (!primerError) {
+      marcarPrimerError(emailInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  } else if (!regExEmails.test(emailInput.value)) {
+    mensajeError += '- El email debe seguir el siguiente formato: ejemplo@ejemplo.com\n';
+    
+    if (!primerError) {
+      marcarPrimerError(emailInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  }
+
+  if (!passwordInput.value) {
+    mensajeError += '- El campo "Contraseña" no puede estar vacío.\n';
+
+    if (!primerError) {
+      marcarPrimerError(passwordInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  } else if (!regExPasswords.test(passwordInput.value)) {
+    mensajeError += '- La contraseña debe tener entre 6 y 10 dígitos. También debe contar al menos con una mayúscula, una minúscula y un dígito.\n';
+    
+    if (!primerError) {
+      marcarPrimerError(passwordInput);
+      primerError = true;
+    }
+
+    esValido = false;
+  }
+
+  // Datos sobre la revista:
+
+  if (!comentario.value) {
+    mensajeError += '- El campo "Comentario" no puede estar vacío.\n';
+
+    if (!primerError) {
+      marcarPrimerError(comentario);
+      primerError = true;
+    }
+
+    esValido = false;
+  } else if (!regExComments.test(comentario.value)) {
+    mensajeError += '- El comentario no puede contar con más de 200 caracteres.\n';
+    
+    if (!primerError) {
+      marcarPrimerError(comentario);
+      primerError = true;
+    }
+
+    esValido = false;
+  }
+
+  if (esValido) {
+    alert('Datos enviados correctamente');
+    form.reset();
+    return true;
+  } else {
+    mensajeError += '\nPor favor, solucione los errores mencionados antes de continuar.'
+    alert(mensajeError);
+    return false;
   }
 }
 
-// !!!!!! Añadir "Por favor, solucione los errores" al mensaje error al final si hay que enviarlo.
+// Añadimos este Event Listener para poder enviar cuando queramos el formulario con la combinación de teclas Ctrl + Intro.
+
+document.addEventListener('keydown', function(event) {
+  if (event.ctrlKey && event.key === 'Enter') {
+    validacion();
+  }
+});
+
+// Añadimos este otro evento para que volvamos al comienzo de la página cuando usemos el botón de reiniciar el formulario.
+
+borrarBtn.addEventListener('click', function() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
