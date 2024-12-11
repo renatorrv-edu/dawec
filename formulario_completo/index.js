44ef5aa5930d070b1d1c8e2b8a1bbf7165d59529
Renato <rromval599@g.educaand.es>
2024-12-11 01:09:11 +0100

    Comenzar index.js

---
+++ formulario_completo/index.js
@@ -0,0 +1,8 @@
+// Recopilamos las áreas de input del formulario.
+
+const form = document.getElementById('form');
+const nameInput = document.getElementById('name');
+const firstLastNameInput = document.getElementById('first_lastname');
+const secondLastNameInput = document.getElementById('second_lastname');
+const addressInput = document.getElementById('address');
+const phoneInput = document.getElementById('phone');
