0f557917690456e7ebb3d992c262d96cae9c428e
Renato <rromval599@g.educaand.es>
2025-01-16 21:01:59 +0100

    Crear directorio ejercicio2

--- dom/ejercicio1/index.js
+++ dom/ejercicio1/index.js
@@ -1,1 +1,19 @@
 document.body.style.backgroundColor = "#FFEE8C";
+document.body.style.fontFamily='Courier'
+document.body.style.margin='40px'
+document.body.style.border='1px solid #f00'
+document.body.style.padding='20px'
+
+//document.body.style.visibility='hidden'
+
+/*setInterval(() => {
+  if (document.body.style.visibility === 'visible') {
+  document.body.style.visibility = 'hidden'
+  } else {
+  document.body.style.visibility = 'visible'
+  }
+ }, 1000)*/
+
+setTimeout(() => {
+  document.body.style.visibility = 'hidden'
+}, 3000)
